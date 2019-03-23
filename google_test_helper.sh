b9edcb6770ae037c475cacce37efd1cc4f4bb862
Oleksandr Kozlov <alxnder.kozlov@gmail.com>
2019-03-23 21:16:22 +0000

    Update helper script.

--- google_test_helper.sh
+++ google_test_helper.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 LIB_NAME="googletest"
+GTEST="gtest"
 SERVER_PREFIX="https://github.com/google/googletest/archive"
 RELEASE_NAME="release-1.8.1"
 EXT=".zip"
 
 mkdir $LIB_NAME
 cd $LIB_NAME
-wget "$SERVER_PREFIX/$RELEASE_NAME$EXT"
-unzip $RELEASE_NAME$EXT
-echo $PWD
-mv "./$LIB_NAME-$RELEASE_NAME/$LIB_NAME/include/gtest/*" "../$LIB_NAME"
-rm -rf "$LIB_NAME-$RELEASE_NAME" $RELEASE_NAME
+
+echo "Fetching $RELEASE_NAME..."
+wget -q "$SERVER_PREFIX/$RELEASE_NAME$EXT" $OUT_REDIR
+
+echo "Unzipping,.."
+unzip -q $RELEASE_NAME$EXT
+
+mv "$LIB_NAME-$RELEASE_NAME/$LIB_NAME/include/$GTEST/" ./
+rm -rf "$LIB_NAME-$RELEASE_NAME" $RELEASE_NAME$EXT
+echo "Done."
 
