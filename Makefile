1ad239044faea8ce20eb0e6a1421108486fcb556
Oleksandr Kozlov <alxnder.kozlov@gmail.com>
2019-03-24 14:39:54 +0000

    Integrate googletest build script.

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 CPP = g++
 RM = rm -f
-CPP_FLAGS = -o -I./googletest/include
+CPP_FLAGS = -o
 OBJECTS := $(patsubst %.cpp,%.o,$(wildcard *.cpp))
 ARTIFACT_NAME = db.o
 
 build: $(OBJECTS)
 	$(CPP) $(CPP_FLAGS) $(ARTIFACT_NAME) $(OBJECTS)
+
+test: basic_test.cpp
+	g++ -std=c++11 -isystem ./googletest/googletest/include/ -pthread basic_test.cpp ./googletest/googletest/libgtest.a -o basic_test.o
 
 clean:
 	$(RM) $(ARTIFACT_NAME)
--- google_test_helper.sh
+++ google_test_helper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# How to build Googletest - http://www.deanbodenham.com/learn/cpp-googletest.html
+
 LIB_NAME="googletest"
-GTEST="gtest"
 SERVER_PREFIX="https://github.com/google/googletest/archive"
 RELEASE_NAME="release-1.8.1"
 EXT=".zip"
@@ -15,7 +16,17 @@ wget -q "$SERVER_PREFIX/$RELEASE_NAME$EXT" $OUT_REDIR
 echo "Unzipping,.."
 unzip -q $RELEASE_NAME$EXT
 
-mv "$LIB_NAME-$RELEASE_NAME/$LIB_NAME/include/$GTEST/" ./
+mv "$LIB_NAME-$RELEASE_NAME/$LIB_NAME/" ./
 rm -rf "$LIB_NAME-$RELEASE_NAME" $RELEASE_NAME$EXT
+
+echo "Building Googletest..."
+cd $LIB_NAME
+g++ -isystem ${PWD}/include -I${PWD} -pthread -c ${PWD}/src/gtest-all.cc 
+ar -rv libgtest.a gtest-all.o
+
+# ls *.a -> should return libgtest.a
+cd ./make
+make
+
 echo "Done."
 
