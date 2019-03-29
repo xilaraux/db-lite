0263f36cbe5d6837d11a159ecf5310ca6b950b44
xilaraux <alxnder.kozlov@gmail.com>
2019-03-29 22:30:32 +0000

    Write tests. Still bad.

--- tests/Utils_test.cpp
+++ tests/Utils_test.cpp
@@ -1,16 +1,30 @@
 #include <gtest/gtest.h>
+
 #include <string>
 using std::string;
 
 #include "../src/Utils.h"
 
 TEST(testUtils, parseInsertStatement){
-	Row* insertRow = new Row;
-	string target = "insert 1 a q";
+	Row* row = new Row;
+	
+	string target = "insert 1 username email@email.com";
 
-	parseInsertStatement(target, *insertRow);
+	parseInsertStatement(target, *row);
 
-	EXPECT_EQ(1, insertRow->id);
+	EXPECT_EQ(1, row->id);
+	EXPECT_STREQ("username", row->username);
+	EXPECT_STREQ("email@email.com", row->email);
+
+	// Second test
+	row = new Row;
+	target = "insert 2   vasya       ema@company.com";
+
+	parseInsertStatement(target, *row);
+
+	EXPECT_EQ(2, row->id);
+	EXPECT_STREQ("vasya", row->username);
+	EXPECT_STREQ("ema@company.com", row->email);
 }
 
 int main(int argc, char* argv[]){
